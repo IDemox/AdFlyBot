af360e96fda70abded8e9b1a348180fb66966bc4
Meep779 <meep779@gmail.com>
2014-12-17 16:38:09 -0800

    fix

--- AddflyBot v1/AddflyBot v1/Form1.vb
+++ AddflyBot v1/AddflyBot v1/Form1.vb
@@ -1,5 +1,7 @@
-﻿Public Class Form1
+﻿
+Imports System.Net
 
+Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         WebBrowser1.Navigate(TextBox1.Text)
     End Sub
